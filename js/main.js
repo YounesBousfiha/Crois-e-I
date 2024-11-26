fbb1652bf8bb479066a8fc095cdb9586c0e0c56b
Coderyounes <younesanas2021@gmail.com>
2024-11-26 10:29:18 +0100

    feat:(FCWA-4):  rend navbar responsive pour la tablet et le mobile

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('burgerMenu').addEventListener('click', function() {
+    let mobileMenu = document.getElementById('mobileMenu');
+    mobileMenu.classList.toggle('hidden');
+});
