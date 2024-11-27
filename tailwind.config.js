c90e839e57673ac4dcb22cd765f2349cbceb5859
Coderyounes <younesanas2021@gmail.com>
2024-11-27 15:21:22 +0100

    feat(FCWA-4): dessin the card player apparence et place la card dans le terrain

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./pages/*.html" ,"./js/*.js"],
+  content: ["./*.html", "./pages/*.html" ,"./js/*.js"],
   theme: {
     extend: {
       fontFamily: {
