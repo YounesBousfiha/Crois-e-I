456f7cd3fd5bf8e5c4fa83239d4865b7b6d1a32b
Coderyounes <younesanas2021@gmail.com>
2024-11-29 15:46:42 +0100

    feat:(FCWA-20):  cree un modal d'affichage de joueur pour chaque position

--- js/terrain.js
+++ js/terrain.js
@@ -6,21 +6,85 @@ function addToTerrain(event) {
     let selectedPlace = event.target.parentElement;
     let storage = JSON.parse(localStorage.getItem('playerDB')) || [];
     let allPositions = ["ST", "LW", "RW", "CDM", "CM", "LB", "RB", "CB", "GK"];
+    let playerContainer = document.getElementById('playersBank');
     
     console.log(selectedPlace);
     let placeholders = [...selectedPlace.classList];
     let availablePosition = placeholders.filter(cls => allPositions.includes(cls));
     
+    playerContainer.innerHTML = ``;
     storage.forEach(player => {
         if(availablePosition.includes(player.position)) {
-            PlayerModal(player);
+            PlayerModal(player, playerContainer);
         }
     });
 }
 
-function PlayerModal(obj) {
-    // onClick on that Player
-    // place holder removed & replaced by the new Player
+function PlayerModal(obj, playerContainer) {
+    let playerFromStorage;
+    if(obj.position == 'GK') {
+        playerFromStorage = `
+        <div class="w-44 h-72 bg-white rounded-lg shadow-md px-2 py-2 m-2 hover:scale-110 hover:duration-500 hover:shadow-lg hover:shadow-black">
+                <img src="${obj.photo}" alt="Player Image" class="w-full h-32 object-cover rounded-t-lg">
+                <div class="p-2 space-y-1">
+                    <p class="text-lg font-bold text-center name">${obj.name}</p>
+                    <div class="flex justify-evenly">
+                        <span class="text-gray-600 text postion">${obj.position}</span>
+                        <span class="text-gray-600 rating">${obj.rating}</span>
+                        <img src="${obj.flag}" alt="nation" width="20">
+                        <img src="${obj.logo}" alt="club" width="20">
+                    </div>
+                    <div class="flex space-x-1">
+                        <span class="text-gray-600">DI</span>
+                        <span class="text-gray-600">HA</span>
+                        <span class="text-gray-600">KI</span>
+                        <span class="text-gray-600">RE</span>
+                        <span class="text-gray-600">SP</span>
+                        <span class="text-gray-600">PO</span>
+                    </div>
+                    <div class="flex justify-around space-x-2">
+                        <span class="text-gray-600 pace">${obj.diving || '00'}</span>
+                        <span class="text-gray-600 shooting">${obj.handling || '00'}</span>
+                        <span class="text-gray-600 passing">${obj.kicking || '00'}</span>
+                        <span class="text-gray-600 dribble">${obj.reflexes || '00'}</span>
+                        <span class="text-gray-600 defense">${obj.speed || '00'}</span>
+                        <span class="text-gray-600 physique">${obj.positioning || '00'}</span>
+                    </div>
+                </div>
+            </div>`;
+    } else {
+        playerFromStorage = `
+        <div class="w-44 h-72 bg-white rounded-lg shadow-md px-2 py-2 m-2 hover:scale-110 hover:duration-500 hover:shadow-lg hover:shadow-black">
+                <img src="${obj.photo}" alt="Player Image" class="w-full h-32 object-cover rounded-t-lg">
+                <div class="p-2 space-y-1">
+                    <p class="font-bold text-center name">${obj.name}</p>
+                    <div class="flex justify-evenly">
+                        <span class="text-gray-600 text postion">${obj.position}</span>
+                        <span class="text-gray-600 rating">${obj.rating}</span>
+                        <img src="${obj.flag}" alt="nation" width="20">
+                        <img src="${obj.logo}" alt="club" width="20">
+                    </div>
+                    <div class="flex space-x-1">
+                        <span class="text-gray-600">PC</span>
+                        <span class="text-gray-600">SH</span>
+                        <span class="text-gray-600">PA</span>
+                        <span class="text-gray-600">DR</span>
+                        <span class="text-gray-600">DF</span>
+                        <span class="text-gray-600">PH</span>
+                    </div>
+                    <div class="flex justify-around space-x-2">
+                        <span class="text-gray-600 pace">${obj.pace || '00'}</span>
+                        <span class="text-gray-600 shooting">${obj.shooting || '00'}</span>
+                        <span class="text-gray-600 passing">${obj.passing || '00'}</span>
+                        <span class="text-gray-600 dribble">${obj.dribbling || '00'}</span>
+                        <span class="text-gray-600 defense">${obj.defending || '00'}</span>
+                        <span class="text-gray-600 physique">${obj.physique || '00'}</span>
+                    </div>
+                </div>
+            </div>`;
+    }
+
+    playerContainer.insertAdjacentHTML('afterbegin' ,playerFromStorage);
 }
 
 function expandPlayer() {}
