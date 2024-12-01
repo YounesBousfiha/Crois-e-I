9f0b1f54feb44c9bdb1e157389d506040d708914
Coderyounes <younesanas2021@gmail.com>
2024-12-01 16:24:23 +0100

    FIX: Fix Close BTN in index Page

--- js/main.js
+++ js/main.js
@@ -4,17 +4,7 @@ document.getElementById('burgerMenu').addEventListener('click', function() {
 });
 
 
-let openModalButton = document.getElementById('terrain');
-let playerPlaceholders = document.querySelectorAll('.player-placeholder');
-let closeModalButton = document.getElementById('closeModal');
 let modal = document.getElementById('modal');
-
-playerPlaceholders.forEach(placeholder => {
-    placeholder.addEventListener('click', () => {
-        modal.classList.remove('hidden');
-    });
-});
-
-closeModalButton.addEventListener('click', () => {
+document.getElementById('closeModal').addEventListener('click', () => {
     modal.classList.add('hidden');
-});
+})
